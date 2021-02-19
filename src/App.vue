8b9823269a58ec8da1c54ca1a61f89d1336a5773
杜嘉豪 <610153956@qq.com>
2021-02-19 14:25:20 +0800

    角色模版属性

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <HelloWorld/>
+  <Calculator/>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
