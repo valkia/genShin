4adb71858fed787d2cba0f7b4cb0445218d53ced
杜嘉豪 <610153956@qq.com>
2021-02-20 18:32:30 +0800

    typescript

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
-  <Calculator/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
 </template>
 
-<script setup lang="ts">
-import Calculator from './components/Calculator.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
 </script>
 
 <style>
